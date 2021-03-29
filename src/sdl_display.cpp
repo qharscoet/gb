7c3822138bbcd11626f0b3ee9014e3f5453309b4
Quentin Harscoët <harscoet.quentin@gmail.com>
2021-03-29 17:06:25 +0200

    Link cable (#2)
    Try to fix inconsistencies

--- src/sdl_display.cpp
+++ src/sdl_display.cpp
@@ -236,13 +236,13 @@ bool SDL_Display::handle_events(Emulator &emu)
 					switch_size(4);
 					break;
 
-
 				case SDL_SCANCODE_0:
 					emu.listen_network();
 					break;
 				case SDL_SCANCODE_1:
 					emu.connect_network();
 					break;
+
 				default:
 					break;
 			}
